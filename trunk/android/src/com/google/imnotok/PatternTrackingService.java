98f65a36879278d858b73b142ae1ab90207f01ab
vytautas@google.com <vytautas@google.com@5afa72e5-c21c-c1cb-0f18-3d448e592853>
2010-08-24 18:41:45 +0000

    Added functionality to launch PatternTrackingService when boot is completed and a notification when PatternTrackingService fails.

--- trunk/android/src/com/google/imnotok/PatternTrackingService.java
+++ trunk/android/src/com/google/imnotok/PatternTrackingService.java
@@ -1,20 +1,68 @@
 package com.google.imnotok;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
-/** 
- * Tracks the events in the system and infers when emergency has happened.
- * When this is the case, sends off an EmergencyIntent to
+/**
+ * Tracks the events in the system and infers when emergency has happened. When
+ * this is the case, sends off an EmergencyIntent to
  * EmergencyNotificationService to invoke the response to the emergency.
  */
 public class PatternTrackingService extends Service {
+  private final String mLogTag = "ImNotOk - PatternTrackingService";
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public void onStart(Intent intent, int startId) {
+    super.onStart(intent, startId);
+    Log.d(mLogTag, "Starting the service");
 
+    if (!this.startTrackingPatterns()) {
+      this.informUserAboutProblemTrackingPatterns();
+      this.stopSelf();
+    }
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  private boolean startTrackingPatterns() {
+    return false;
+  }
+
+  private void informUserAboutProblemTrackingPatterns() {
+    // Show a notification to the user that we were unable to start the pattern
+    // tracker.
+    NotificationManager notificationManager =
+        (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
+    
+    Intent retryLaunchingPatternTrackerIntent =
+        new Intent(this, this.getClass());
+    PendingIntent pendingIntent = PendingIntent.getService(
+        this,
+        0,
+        retryLaunchingPatternTrackerIntent,
+        0);
+    
+    Notification notification = new Notification(
+        android.R.drawable.stat_notify_error,
+        this.getString(R.string.unable_to_track_actions_for_emergency_title),
+        System.currentTimeMillis());
+    // Notification should be canceled when clicked    
+    notification.flags |= Notification.FLAG_AUTO_CANCEL;
+    notification.setLatestEventInfo(
+        this,
+        this.getString(R.string.unable_to_track_actions_for_emergency_title),
+        this.getString(R.string.unable_to_track_actions_for_emergency_msg),
+        pendingIntent);
+        
+    notificationManager.notify(0, notification);
+  }
 }
