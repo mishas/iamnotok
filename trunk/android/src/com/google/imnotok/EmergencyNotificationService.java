5ede74efe1c93ddcca50add45d70dcb153aeb33c
vytautas@google.com <vytautas@google.com@5afa72e5-c21c-c1cb-0f18-3d448e592853>
2010-09-11 11:49:32 +0000

    Fixing the issues with disabling the emergency response and deadlock while waiting for the GPS fix.

--- trunk/android/src/com/google/imnotok/EmergencyNotificationService.java
+++ trunk/android/src/com/google/imnotok/EmergencyNotificationService.java
@@ -38,11 +38,10 @@ public class EmergencyNotificationService extends Service {
    * 10 seconds.
    */
   public final static String SHOW_NOTIFICATION_WITH_DISABLE = "showNotification";
-  private final static String STOP_EMERGENCY = "stopEmergency";
+  private final static String STOP_EMERGENCY_INTENT = "com.google.imnotok.STOP_EMERGENCY";
   /** Time allowed for user to cancel the emergency response. */
   private final static int waitForMs = 20000;
-  
-  private Object mEmergencyResponseLock = new Object();
+
   private boolean mEmergencyResponseShouldBeDisabled = false;
   private boolean mServiceRunning = false;
   private int mNotificationID = 0;
@@ -57,26 +56,17 @@ public class EmergencyNotificationService extends Service {
   public void onStart(Intent intent, int startId) {
     Log.d(mLogTag, "onStart() called");
 
-    // Check if the intent was called to stop the emergency.
-    boolean stopEmergency = intent.getBooleanExtra(STOP_EMERGENCY, false);
-    if (stopEmergency) {
-      Log.d(mLogTag, "Disabling the response");
-      this.setEmergencyFlagValue(false);
-      mServiceRunning = false;
-      return;
-    }
     if (!mServiceRunning) {
       Log.d(mLogTag, "Starting the service");
       mServiceRunning = true;
-      boolean showNotification =
-          intent.getBooleanExtra(SHOW_NOTIFICATION_WITH_DISABLE, false);
+      boolean showNotification = intent.getBooleanExtra(SHOW_NOTIFICATION_WITH_DISABLE, false);
       this.setEmergencyFlagValue(true);
-      
+
       // Start location tracker from here since it takes some time to get
       // the first GPS fix.
       mLocation = null;
       this.startLocationTracker();
-      
+
       if (showNotification) {
         this.showDisableNotificationAndWaitToInvokeResponse();
       } else {
@@ -87,115 +77,104 @@ public class EmergencyNotificationService extends Service {
       Log.d(mLogTag, "Service already running");
     }
   }
-  
+
   /**
    * Sends a sms to another device
    **/
   private void sendTextMessage() {
-	Thread messageSender = new Thread(new Runnable() {
-  	  @Override
-	  public void run() {
-  	    Log.d(mLogTag, "Sending sms");
-  	    String phoneNumber = "5556";
-  	    //---TODO: Include the GPS location in the message.
-  	    // Location loc = getLocation();
-  	    // Log.d(mLogTag, "Sending the location: latitude: " + loc.getLatitude() + ", longitude: " + loc.getLongitude());
-  	    String message = "I am not OK!";
-  	    
-  	    String SENT = "SMS_SENT";
-  	    String DELIVERED = "SMS_DELIVERED";
-  	 
-  	    PendingIntent sentPI = PendingIntent.getBroadcast(EmergencyNotificationService.this, 0,
-  	        new Intent(SENT), 0);
-  	    PendingIntent deliveredPI = PendingIntent.getBroadcast(EmergencyNotificationService.this, 0,
-  	        new Intent(DELIVERED), 0);
-  	 
-  	        //---when the SMS has been sent---
-  	    registerReceiver(new BroadcastReceiver(){
-  	        @Override
-  	        public void onReceive(Context arg0, Intent arg1) {
-  	            switch (getResultCode())
-  	            {
-  	                case Activity.RESULT_OK:
-  	                    Toast.makeText(getBaseContext(), "SMS sent", 
-  	                            Toast.LENGTH_SHORT).show();
-  	                    break;
-  	                case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-  	                    Toast.makeText(getBaseContext(), "Generic failure", 
-  	                            Toast.LENGTH_SHORT).show();
-  	                    break;
-  	                case SmsManager.RESULT_ERROR_NO_SERVICE:
-  	                    Toast.makeText(getBaseContext(), "No service", 
-  	                            Toast.LENGTH_SHORT).show();
-  	                    break;
-  	                case SmsManager.RESULT_ERROR_NULL_PDU:
-  	                    Toast.makeText(getBaseContext(), "Null PDU", 
-  	                            Toast.LENGTH_SHORT).show();
-  	                    break;
-  	                case SmsManager.RESULT_ERROR_RADIO_OFF:
-  	                    Toast.makeText(getBaseContext(), "Radio off", 
-  	                                Toast.LENGTH_SHORT).show();
-  	                        break;
-  	                }
-  	            }
-  	        }, new IntentFilter(SENT));
-  	 
-  	        //---when the SMS has been delivered---
-  	    registerReceiver(new BroadcastReceiver(){
-  	        @Override
-  	        public void onReceive(Context arg0, Intent arg1) {
-  	            switch (getResultCode())
-  	            {
-  	                case Activity.RESULT_OK:
-  	                    Toast.makeText(getBaseContext(), "SMS delivered", 
-  	                            Toast.LENGTH_SHORT).show();
-  	                    break;
-  	                case Activity.RESULT_CANCELED:
-  	                    Toast.makeText(getBaseContext(), "SMS not delivered", 
-  	                                Toast.LENGTH_SHORT).show();
-  	                        break;                        
-  	                }
-  	            }
-  	        }, new IntentFilter(DELIVERED));        
-  	 
-  	        SmsManager sms = SmsManager.getDefault();
-  	        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);        
-			
-	  }
-	});
-	messageSender.start();
-  } 
-  
-  
+    Thread messageSender = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        Log.d(mLogTag, "Sending sms");
+        String phoneNumber = "5556";
+        Location loc = getLocation();
+        Log.d(mLogTag, "Sending the location - latitude: " + loc.getLatitude() + ", longitude: "
+            + loc.getLongitude());
+        String message =
+            "I am not OK! My current location is: latitude " + loc.getLatitude() + ", longitude "
+                + loc.getLongitude();
+
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI =
+            PendingIntent.getBroadcast(EmergencyNotificationService.this, 0, new Intent(SENT), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(
+            EmergencyNotificationService.this, 0, new Intent(DELIVERED), 0);
+
+        // ---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver() {
+          @Override
+          public void onReceive(Context arg0, Intent arg1) {
+            switch (getResultCode()) {
+              case Activity.RESULT_OK:
+                Toast.makeText(getBaseContext(), "SMS sent", Toast.LENGTH_SHORT).show();
+                break;
+              case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                Toast.makeText(getBaseContext(), "Generic failure", Toast.LENGTH_SHORT).show();
+                break;
+              case SmsManager.RESULT_ERROR_NO_SERVICE:
+                Toast.makeText(getBaseContext(), "No service", Toast.LENGTH_SHORT).show();
+                break;
+              case SmsManager.RESULT_ERROR_NULL_PDU:
+                Toast.makeText(getBaseContext(), "Null PDU", Toast.LENGTH_SHORT).show();
+                break;
+              case SmsManager.RESULT_ERROR_RADIO_OFF:
+                Toast.makeText(getBaseContext(), "Radio off", Toast.LENGTH_SHORT).show();
+                break;
+            }
+          }
+        }, new IntentFilter(SENT));
+
+        // ---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver() {
+          @Override
+          public void onReceive(Context arg0, Intent arg1) {
+            switch (getResultCode()) {
+              case Activity.RESULT_OK:
+                Toast.makeText(getBaseContext(), "SMS delivered", Toast.LENGTH_SHORT).show();
+                break;
+              case Activity.RESULT_CANCELED:
+                Toast.makeText(getBaseContext(), "SMS not delivered", Toast.LENGTH_SHORT).show();
+                break;
+            }
+          }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+
+      }
+    });
+    messageSender.start();
+  }
+
+
   /**
    * Sends an email
    */
   private void sendEmail() {
     // TODO(raquelmendez): chose the correct contact to send the email to
     String contact = "raquel.mendez.gomez@gmail.com";
-    
+
     final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
     emailIntent.setType("plain/text");
     emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, contact);
-    emailIntent.putExtra(
-        android.content.Intent.EXTRA_SUBJECT,R.string.i_am_not_ok_start);
-    emailIntent.putExtra(
-        android.content.Intent.EXTRA_TEXT, R.string.i_am_not_ok_start);
+    emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, R.string.i_am_not_ok_start);
+    emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, R.string.i_am_not_ok_start);
     Intent.createChooser(emailIntent, "Send mail...");
-      
-    
   }
 
   private void invokeEmergencyResponse() {
     Log.d(mLogTag, "Invoking emergency response");
-    
+
     // Check the user preferences:
     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
     // If the user has enable the sms notifications, send them:
-    if(prefs.getBoolean(getString(R.string.checkbox_sms_notification), true)) {
+    if (prefs.getBoolean(getString(R.string.checkbox_sms_notification), true)) {
       Log.d(mLogTag, "Sending sms...");
-      sendTextMessage(); 
-    }       
+      sendTextMessage();
+    }
     sendEmail();
     // TODO(raquelmendez): Do we want to inform the ways that are desactivated?
     mServiceRunning = false;
@@ -203,30 +182,42 @@ public class EmergencyNotificationService extends Service {
 
   private void showDisableNotificationAndWaitToInvokeResponse() {
     Log.d(mLogTag, "Showing notification and waiting");
-    
+
     // Reset the flag.
     setEmergencyFlagValue(true);
-    
+
     // Show a notification.
     final NotificationManager notificationManager =
         (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
 
-    Intent disableEmergencyIntent = new Intent(this, this.getClass());
-    disableEmergencyIntent.putExtra(STOP_EMERGENCY, true);
-    PendingIntent pendingIntent =
-        PendingIntent.getService(this, 0, disableEmergencyIntent, 0);    
+    Intent disableEmergencyIntent = new Intent(STOP_EMERGENCY_INTENT);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, disableEmergencyIntent, 0);
 
-    Notification notification = new Notification(android.R.drawable.stat_sys_warning,
-        this.getString(R.string.emergency_response_starting),
-        System.currentTimeMillis());
+    Notification notification =
+        new Notification(android.R.drawable.stat_sys_warning,
+            this.getString(R.string.emergency_response_starting), System.currentTimeMillis());
     // Notification should be canceled when clicked
     notification.flags |= Notification.FLAG_AUTO_CANCEL | Notification.FLAG_ONGOING_EVENT;
-    notification.setLatestEventInfo(this,
-        this.getString(R.string.emergency_response_starting),
+    notification.setLatestEventInfo(this, this.getString(R.string.emergency_response_starting),
         this.getString(R.string.click_to_disable), pendingIntent);
-    
+
     notificationManager.notify(mNotificationID, notification);
-    
+
+    // Register a receiver that can receive the cancellation intents.
+    BroadcastReceiver cancellationReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        Log.d(mLogTag, "Received cancellation intent...");
+        // TODO(vytautas): check the state of the service when continuous mode
+        // is introduced.
+        EmergencyNotificationService.this.setEmergencyFlagValue(false);
+        // TODO(vytautas): make this multi-thread friendly.
+        EmergencyNotificationService.this.mServiceRunning = false;
+      }
+    };
+    IntentFilter intentFilter = new IntentFilter(STOP_EMERGENCY_INTENT);
+    this.registerReceiver(cancellationReceiver, intentFilter);
+
     // Start the waiting in a separate thread since otherwise the service will
     // not be able to receive the intent for cancelling the emergency response.
     Thread waiterThread = new Thread(new Runnable() {
@@ -254,65 +245,62 @@ public class EmergencyNotificationService extends Service {
   private synchronized boolean getEmergencyFlagValue() {
     return mEmergencyResponseShouldBeDisabled;
   }
-  
+
   private synchronized void setLocation(Location location) {
-	mLocation = location;
+    mLocation = location;
+    this.notifyAll();
   }
-  
-  /** Should be called from a separate thread since may block waiting for
+
+  /**
+   * Should be called from a separate thread since may block waiting for
    * location.
    */
   @SuppressWarnings("unused")
-private synchronized Location getLocation() {
-	// Construct a copy of the current location.
-	while (mLocation == null) {
-	  try {
-		synchronized (mEmergencyResponseLock) {
-		  mEmergencyResponseLock.wait();
-		}
-	  } catch (InterruptedException e) {
- 	    e.printStackTrace();
-	  }
-	}
-	return new Location(mLocation);
+  private synchronized Location getLocation() {
+    // Construct a copy of the current location.
+    while (mLocation == null) {
+      try {
+        Log.d(mLogTag, "Waiting for location");
+        this.wait();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    return new Location(mLocation);
   }
-  
+
   private void startLocationTracker() {
-	// TODO(vytautas): get permission to change settings and make sure we
-	// can turn the GPS on even if the user has disabled it.
-	LocationManager mlocManager =
-	  (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-	LocationListener mlocListener = new UserLocationListener();
-	mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+    // TODO(vytautas): get permission to change settings and make sure we
+    // can turn the GPS on even if the user has disabled it.
+    LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    LocationListener mlocListener = new UserLocationListener();
+    mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
   }
-  
+
   private class UserLocationListener implements LocationListener {
-	@Override
-	public void onLocationChanged(Location location) {
-	  Log.d(mLogTag, "Location has changed");
-	  if (EmergencyNotificationService.this.mLocation != null) {
-		if (location.getAccuracy() < EmergencyNotificationService.this.mLocation.getAccuracy()) {
-		  EmergencyNotificationService.this.setLocation(location);
-		}
-	  } else {
-		EmergencyNotificationService.this.setLocation(location);
-		synchronized (mEmergencyResponseLock) {
-		  mEmergencyResponseLock.notifyAll();
-		}
-	  }
-	}
+    @Override
+    public void onLocationChanged(Location location) {
+      Log.d(mLogTag, "Location has changed");
+      if (EmergencyNotificationService.this.mLocation != null) {
+        if (location.getAccuracy() < EmergencyNotificationService.this.mLocation.getAccuracy()) {
+          EmergencyNotificationService.this.setLocation(location);
+        }
+      } else {
+        EmergencyNotificationService.this.setLocation(location);
+      }
+    }
 
-	@Override
-	public void onProviderDisabled(String provider) {		
-	}
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
 
-	@Override
-	public void onProviderEnabled(String provider) {
-	}
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
 
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	}
-	  
-  }  
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+  }
 }
